aadce9afff90ed0dcff6bb12f60e0189fca51855
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-12-18 17:44:26 +0600

    add service

--- src/Pages/Dashboard/AddServices.js
+++ src/Pages/Dashboard/AddServices.js
@@ -1,9 +1,38 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const AddServices = () => {
+  const [imageUrl, setImageUrl] = useState("");
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  // 84a5698c1163075e540df1dc6008c8cf  imgbb api key
+  const uploadImage = (event) => {
+    const image = event.target.files[0];
+
+    const formData = new FormData();
+
+    formData.set("image", image);
+    axios
+      .post(
+        "https://api.imgbb.com/1/upload?key=84a5698c1163075e540df1dc6008c8cf",
+        formData
+      )
+      .then((res) => setImageUrl(res.data.data.url));
+  };
+
+  const onSubmit = (data) => {
+    const services = {
+      ...data,
+      image: imageUrl,
+    };
+    console.log(services);
+    // axios
+    //   .servicesApi("/add-services", services)
+    //   .then((res) => console.log(res));
+  };
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -33,7 +62,11 @@ const AddServices = () => {
               ></textarea>
             </div>
             <div className="form-control mt-3">
-              <input type="file" className="file-input file-input-bordered" />
+              <input
+                onChange={uploadImage}
+                type="file"
+                className="file-input file-input-bordered"
+              />
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Add Service</button>
